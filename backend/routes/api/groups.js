e6de355eb682b38c7f7be9a306bb5f9248f6ab64
KMulloth <k.mulloth@gmail.com>
2022-05-08 15:43:47 -0500

    working example

--- backend/routes/api/groups.js
+++ backend/routes/api/groups.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const asyncHandler = require("express-async-handler");
+const { setTokenCookie, requireAuth } = require("../../utils/auth.js");
 
 const { User, Group, Event } = require("../../db/models");
 
